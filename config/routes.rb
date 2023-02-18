b2f269866bc6c72cc55a373be7670da5b94c4bef
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-02-18 14:43:16 +0000

    [add]deviseコントローラ作成_アクションも作成_課題提出の内容部分は未完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
     # 顧客用
