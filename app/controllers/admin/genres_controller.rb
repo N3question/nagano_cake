729ae4323df0a00afb9479b9537ed0c152fac388
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-02-21 15:03:44 +0000

    [add]ジャンル一覧の作成完了

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,11 +5,22 @@ class Admin::GenresController < ApplicationController
   end
   
   def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to '/admin/genres'
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
   
   def update
   end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "homes#top"
-  get '/', to: 'homes#top', as: 'top'
-  get '/about', to: 'homes#about', as: 'about'
-  resources :items, only: [:index, :show]
-  get '/customers/my_page', to: 'customers#show', as: 'my_page'
-  get '/customers/information/edit', to: 'customers#edit', as: 'edit_information'
-  patch '/customers/information', to: 'customers#update', as: 'information'
-  get '/customers/unsubscribe', to: 'customers#unsubscribe', as: 'unsubscribe'
-  patch '/customers/withdraw', to: 'customers#withdraw', as: 'withdraw'
-  resources :cart_items, only: [:index, :update, :destroy, :create]
-  delete '/cart_items/destroy_all'
-  resources :orders, only: [:new, :create, :index, :show]
-  post '/orders/comfirm', to: 'orders#comfirm'
-  get '/orders/complete', to: 'orders#complete'
-  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  
+  scope module: :public do
+    root "homes#top"
+    get '/', to: 'homes#top', as: 'top'
+    get '/about', to: 'homes#about', as: 'about'
+    resources :items, only: [:index, :show]
+    get '/customers/my_page', to: 'customers#show', as: 'my_page'
+    get '/customers/information/edit', to: 'customers#edit', as: 'edit_information'
+    patch '/customers/information', to: 'customers#update', as: 'information'
+    get '/customers/unsubscribe', to: 'customers#unsubscribe', as: 'unsubscribe'
+    patch '/customers/withdraw', to: 'customers#withdraw', as: 'withdraw'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete '/cart_items/destroy_all'
+    resources :orders, only: [:new, :create, :index, :show]
+    post '/orders/comfirm', to: 'orders#comfirm'
+    get '/orders/complete', to: 'orders#complete'
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
     
   
   namespace :admin do
-    # root "sessions#new"
     get '/', to: 'homes#top', as: 'top'
     resources :items, except: :destroy
     resources :genres, only: [:index, :create, :edit, :update]
