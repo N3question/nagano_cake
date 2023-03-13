5dee89ff0a8e6baecb906a1ca5f062a72e0777ab
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-03-13 13:44:26 +0000

    [add]商品一覧画面＆商品詳細画面の作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ApplicationRecord
   
   has_one_attached :image
-  # validates is_active, inclusion: [true, false]
   belongs_to :genre # アソシエーション
+  
+  # 消費税を加えた商品価格
+  def add_tax_price
+      (self.price * 1.1).round
+  end
   
 end
