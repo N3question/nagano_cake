b30a02f1d1b526f8ba53df8d298cd989a49155fd
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-03-30 21:31:31 +0000

    [fix]itemのis_activeのエラー修正途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -31,8 +31,11 @@ class Admin::ItemsController < ApplicationController
   
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to admin_items_path
+    if @item.update(item_params)
+      redirect_to admin_items_path
+    else
+      render 'edit'
+    end
   end
   
   # 任意アクション
